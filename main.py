f0e66dbd5dda4cbfcd2b20595e5a07d304d6d158
berkay <143879221+berkaygldal@users.noreply.github.com>
2025-03-21 17:40:50 +0300

    Update and rename projectseaside.py to main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+import getpass
+import shlex
+import importlib.util
+import sys
+from colorama import Fore, Style
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PACKAGE_DIR = os.path.join(BASE_DIR, "packages")
+sys.path.append(PACKAGE_DIR)
+
+def load(file):
+    filepath = os.path.join(PACKAGE_DIR, f"{file}.py")
+    if os.path.exists(filepath):
+        spec = importlib.util.spec_from_file_location(file, filepath)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        return module
+    return None
+while True:
+    os.system("title seaside : " + getpass.getuser() + "@" + os.getcwd().split("\\")[-1])
+    x = input(Fore.MAGENTA + getpass.getuser() + "@" + os.getcwd().split("\\")[-1] + Fore.WHITE + " :" + Fore.GREEN + " $ " + Style.RESET_ALL)
+    if not x in {"exit", "powershell", "cmd"} and (parts := shlex.split(x)):
+        cmd = parts[0].lower()
+        if cmd in {"cat", "fileexists", "filesize", "isdir", "isfile", "cwd", "rename", "move", "mkdir", "rm", "copy", "ls", "cd", "info", "help", "clear", "exit"}:
+            code = load(cmd)
+            if code and hasattr(code, cmd):
+                getattr(code, cmd)(*parts[1:])
+        elif os.path.exists(os.path.join(PACKAGE_DIR, f"{cmd}.py")):
+            code = load(cmd)
+            if code and hasattr(code, cmd):
+                getattr(code, cmd)(*parts[1:])
+        else:
+            print("seaside : Command not found, you can use 'help' to list all commands.")
+    elif x in {"exit", "powershell", "cmd"}:
+        break
