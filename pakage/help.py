cad8eb17c68d8d39d951eac6c54f9cd11cd0da60
berkay <143879221+berkaygldal@users.noreply.github.com>
2025-03-23 12:54:25 +0300

    Update help.py

--- pakage/help.py
+++ pakage/help.py
@@ -4,4 +4,4 @@ from colorama import Fore, Style
 install = os.path.dirname(os.path.abspath(__file__))
 
 def help():
-    print(" " + str(os.listdir(os.path.join(install, "packages"))).replace("'", "").replace("[", "").replace("]", "").replace(".py", "").replace(", __pycache__", ""))
+    print(" " + str(os.listdir(os.path.join(install))).replace("'", "").replace("[", "").replace("]", "").replace(".py", "").replace(", __pycache__", ""))
